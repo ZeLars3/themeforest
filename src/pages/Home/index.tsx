a4592f2e94a5b4d22c02d4f0bb1f30caadef92c9
ZeLars <zelt378@gmail.com>
2022-10-20 12:31:55 +0300

    feature/add Basic info block

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,11 +6,13 @@ import { Business } from 'components/Business'
 import { SuccessStatistic } from 'components/SuccessStatistic'
 import { NewSolutions } from 'components/NewSolutions'
 import { Benefits } from 'components/Benefits'
+import { BasicInfo } from 'components/BasicInfo'
 
 export const Home: FC = () => {
   return (
     <>
       <MainLayout>
+        <BasicInfo />
         <Business />
         <NewSolutions />
         <SuccessStatistic />
